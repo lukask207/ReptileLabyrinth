eedbca941e16a5506c80ce50ed89e8bade0e8692
lukask207 <>
2023-08-27 18:30:06 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
+
+#Reptile_Labyrinth
+
+Reptile Labyrith is a python termial game where users tries to escape the labyrith in as few steps as possible.
+Everytime the reptile hits a hidden trap it needs to start over. 
+
+# How to play
+
+First the player needs to choose a name for the reptile and the labyrinth is radomly generated. The reptile is displayed in the labyrith with the first letter its name. The retile will always start in the top left corner of the labyrith. 
+Home, displayed with the letter H, is displayed in one of the space in bottom right corner of the labyrinth. The user moves the reptile using the letters E, W, N and S.
+The user needs to get to Home with as few steps as possible. Hidden in the labyrinth are a number of traps. Every time the reptile hits a trap the reptile moves back to the start position while the number to steps keeps adding up.
+
+# Existing Features
+
+Random board generation.
+    The traps are randomly placed in the labyrinth.
+    The user can not see where the traps are.
+    Home is randomly place within the four bottom right spaces on the board.
+    Traps can not be placed in the start position nor in the home position.
+
+Accepts user input
+Input validation. The class method only reacts on N, S, E, W
+
+# Future Features
+Leader board
+
+
+
+# Bugs resolved 
+- N/S/E/W directions did not move the reptile in correct direction. This has been resolved.
+- Number of steps were counted even if other then N, S, E or W was inputted. This has been resolved.
+- If traps were located in the same positions as 'start' or 'home' position, less traps were active
+  in the game. This has been resolved.
+
+
+# Manual test Plan
+- Verified that the reptile moves in the correct directions by entering N/S/E/W.
+- Verified that #steps is correct by counting N/S/E/W.
+- Verfied that input data other then N/S/E/W (both lower and upper case) is not counted
+  in the number of steps.
+- Verified that the name of the reptile is capilized.
+- Verified that first letter of the name of the reptile is printed in the labyrinth.
+- Verified that the game starts over if the reptile moves outside the labyrinth.
+- Verified that correct text is displayed when executing the game and at the end of the game.
+- Verified that a trap is hit.
+- Verified that three traps exist in the game by adding print statements afer creating of the traps.
+- Verified that the game ends of Home is reached.
+
+
+# Validator testing
+PEP8 no errors returned
+
+
+
+
+
 ![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
 Welcome USER_NAME,
--- run.py
+++ run.py
@@ -1,3 +1,105 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+class Reptile:
+    """
+    Reptile class which sets the start position
+    and moves the reptile depending on N(orth),
+    S(outh), E(ast) and W()west input from the user
+    """
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.steps = 0
+
+    def walk_direction(self, direction):
+        """North and South are in y direction,
+           East and West are in X direction"""
+        if direction == 'N':
+            self.y -= 1
+        elif direction == 'S':
+            self.y += 1
+        elif direction == 'E':
+            self.x += 1
+        elif direction == 'W':
+            self.x -= 1
+        """" Only increase steps if direction is input
+             as N/S/E/W character """
+        if (direction == 'N' or direction == 'S' or
+           direction == 'E' or direction == 'W'):
+            self.steps += 1
+
+    def display_labyrinth(self):
+        """ Print reptile name initial and H for home of
+            the reptile """
+        Reptile_initial = self.name[0]
+        for grid_y in range(7):
+            for grid_x in range(7):
+                if grid_x == reptile.x and grid_y == reptile.y:
+                    print(Reptile_initial, end='  ')
+                elif grid_x == home.x and grid_y == home.y:
+                    print("H", end='  ')
+                else:
+                    print(".", end='  ')
+            print()
+
+
+class Home:
+    """
+    Reptile home position
+    """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+"""
+Initialize Reptile, start position, and locations of the Trap
+Re-define name of reptile to start with capital
+"""
+
+reptile_name = input("Name your reptile: ").capitalize()
+reptile = Reptile(reptile_name, 0, 0)
+
+""" Position home at the right bottom area of the labyrinth """
+home = Home((random.randint(4, 6)), (random.randint(4, 6)))
+""" Generate random positions for three traps
+    and make sure (0,0) and Home are not part of the trap positions
+"""
+traps = [(random.randint(0, 6), random.randint(0, 6)) for i in range(3)]
+while ((0, 0) or (home.x, home.y)) in traps:
+    traps = [(random.randint(0, 6), random.randint(0, 6)) for i in range(3)]
+
+
+"""
+    Start of Reptile Labyrinth Game
+
+    Traps are not re-positioned until a new game is started
+    It is not allowed to move outside the labyrinth
+"""
+while reptile.x != home.x or reptile.y != home.y:
+    print(f"{reptile.name} is at ({reptile.x}, {reptile.y})")
+
+    """ Check if the reptile is outside the labyrinth """
+    if reptile.x < 0 or reptile.x > 6 or reptile.y < 0 or reptile.y > 6:
+        print("You're outside the labyrinth! Start Over!")
+        reptile.x = 0
+        reptile.y = 0
+
+    """ Check for traps """
+    for ind in traps:
+        if reptile.x == ind[0] and reptile.y == ind[1]:
+            print("OOPS, you were trapped! Start over!")
+            reptile.x = 0
+            reptile.y = 0
+
+    """ Move the lizard """
+    reptile.display_labyrinth()
+    direction = input('Which direction do '
+                      'you want to walk? (N/S/E/W): ').upper()
+    reptile.walk_direction(direction)
+
+""" Print message at the end of the game """
+print(f'Well done {reptile.name}!')
+print(f'You came safely home in {reptile.steps} steps!')
